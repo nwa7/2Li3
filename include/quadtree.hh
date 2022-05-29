e77802022d6081e313c19cc53b98093e2ea56cb2
Juliette <juliette.jeannin@hotmail.fr>
2022-05-29 12:47:56 +0200

    Ju Quadtree

--- include/quadtree.hh
+++ include/quadtree.hh
@@ -3,26 +3,35 @@
 
 #include "bloc.hh"
 
-class Quadtree{
+class Quadtree {
     
     public :
 
+    // Variables
     int xmin, xmax, ymin, ymax;
 
-    Bloc* platforms[5];
-    int ind=0;
+    std::vector<Bloc> blocs;
 
-    Quadtree* upleft;
-    Quadtree* downleft;
-    Quadtree* upright;
-    Quadtree* downright;
+    Quadtree* upleft = nullptr;
+    Quadtree* downleft = nullptr;
+    Quadtree* upright = nullptr;
+    Quadtree* downright = nullptr;
 
+    // Methodes
+
+    Quadtree(int xmin, int xmax, int ymin, int ymax);
     void generate(Bloc* map);
+
+    int getX();
+    int getY();
+    bool isLeaf();
+    bool isFull();
+
     void split();
-    //Bloc* searchLeaf(float x, float y);
     int searchLeaf(Bloc* obj);
     void insertBloc(Bloc* obj);
-    Bloc* collision(Bloc* returnObjets[], Bloc* bloc);
+
+    std::vector<Bloc> searchBloc(Bloc* obj);
 
 };
 
--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -4,190 +4,146 @@ void Quadtree::generate(Bloc* map){
 
     int subWidth = (int) (map->width/2);
     int subHeight = (int) (map->height/2);
-    int x = (int) map->pos.x;
-    int y = (int) map->pos.y;
     
     // Initialisation racine
     this->xmin=-subWidth;
     this->xmax=subWidth;
     this->ymin=-subHeight;
     this->ymax=subHeight;
-    this->platforms[0]=map;
-    this->ind++;
+}
 
-    // Initialisation upleft
-    this->upleft->xmin=x-subWidth;
-    this->upleft->xmax=x;
-    this->upleft->ymin=y;
-    this->upleft->ymax=y+subHeight;
-
-    // Initialisation downleft
-    this->downleft->xmin=x-subWidth;
-    this->downleft->xmax=x;
-    this->downleft->ymin=y-subHeight;
-    this->downleft->ymax=y;
-
-    // Initialisation upright
-    this->upright->xmin=x;
-    this->upright->xmax=x+subWidth;
-    this->upright->ymin=y;
-    this->upright->ymax=y+subHeight;
-    
-    // Initialisation downright
-    this->downright->xmin=x;
-    this->downright->xmax=x+subWidth;
-    this->downright->ymin=y-subHeight;
-    this->downright->ymax=y;
-
+/* Recup infos */
+int Quadtree::getX() {
+    return (this->xmin+this->xmax)/2;
+}
+int Quadtree::getY() {
+    return (this->ymin+this->ymax)/2;
+}
+bool Quadtree::isLeaf() {
+    return (this->upleft==nullptr && this->upright==nullptr && this->downleft==nullptr && this->downright==nullptr);
+}
+bool Quadtree::isFull() {
+    return (blocs.size()>4);
 }
 
 void Quadtree::split() {
-    int subWidth = (int) ((this->xmax - this->xmin)/2);
-    int subHeight = (int) ((this->ymax - this->ymin)/2);
-    int x = (int) ((this->xmax + this->xmin)/2);
-    int y = (int) ((this->ymax + this->ymin)/2);
-
     // Upleft
-    this->upleft->xmin=x-subWidth;
-    this->upleft->xmax=x;
-    this->upleft->ymin=y;
-    this->upleft->ymax=y+subHeight;
-
+    this->upleft = new Quadtree(this->xmin, this->getX(), this->getY(), this->ymax);
     // Downleft
-    this->downleft->xmin=x-subWidth;
-    this->downleft->xmax=x;
-    this->downleft->ymin=y-subHeight;
-    this->downleft->ymax=y;
-
+    this->downleft = new Quadtree(this->xmin, this->getX(),this->ymin, this->getY());
     // Upright
-    this->upright->xmin=x;
-    this->upright->xmax=x+subWidth;
-    this->upright->ymin=y;
-    this->upright->ymax=y+subHeight;
-    
+    this->upright = new Quadtree(this->getX(), this->xmax, this->getY(), this->ymax);
     // Downright
-    this->downright->xmin=x;
-    this->downright->xmax=x+subWidth;
-    this->downright->ymin=y-subHeight;
-    this->downright->ymax=y;
+    this->downright = new Quadtree(this->getX(), this->xmax, this->ymin, this->getY());
 }
 
+/* Recherche de la partie du quadtree dans laquelle va etre range le bloc */
 int Quadtree::searchLeaf(Bloc* obj){
-    int pos_x = (int) ((this->xmax + this->xmin)/2);
-    int pos_y = (int) ((this->ymax + this->ymin)/2);
-
-    int index = -1; // Si objet appartient au parent
-    bool up = false;
-    bool down = false;
-
-    // Test si objet est entièrement dans cadran du haut
-    if(obj->getPosY() > pos_y &&  obj->getPosY() + obj->getHeight()/2 > pos_y) {
-        up = true;
-    }
-    
-    // Test si objet est entièrement dans cadran du bas
-    else if(obj->getPosY() < pos_y &&  obj->getPosY() + obj->getHeight()/2 < pos_y) {
-        down = true;
-    }
-
-    // Test si objet est entièrement dans cadran de gauche
-    if(obj->getPosX() < pos_x &&  obj->getPosX() + obj->getWidth()/2 < pos_x) {
-        
-        // En haut
-        if(up) {
-            index = 1;
+    int index;
+    if(!this->isLeaf()) {
+        // Teste si objet dans cadran du haut
+        if(obj->getPosY() > this->getY()) {
+            // A gauche
+            if (obj->getPosX() < this->getX()) {
+                index = 1;
+            }
+            //A droite
+            else {
+                index = 2;
+            }
         }
         
-        // En bas
-        else if(down) {
-            index = 3;
-        }
-    }
-
-    // Test si objet est entièrement dans cadran de droite
-    else if(obj->getPosX() > pos_x &&  obj->getPosX() + obj->getWidth()/2 > pos_x) {
-        
-        // En haut
-        if(up) {
-            index = 2;
-        }
-        
-        // En bas
-        else if(down) {
-            index = 4;
+        // Test si objet dans cadran du bas
+        else {
+            // A gauche
+            if (obj->getPosX() < this->getX()) {
+                index = 3;
+            }
+            //A droite
+            else {
+                index = 4;
+            }
         }
     }
     return index;
 }
 
 void Quadtree::insertBloc(Bloc* obj) {
-    if(this->downleft !=nullptr) {
-        int index = this->searchLeaf(obj);
+    int index = this->searchLeaf(obj);
 
-        // L'objet appartient à un enfant
-        if(index != -1) {
-
-            // Haut-gauche
-            if(index==1) {
-                this->upleft->insertBloc(obj);
-                return;
-            }
-            // Haut-doite
-            else if(index==2) {
-                this->upright->insertBloc(obj);
-                return;
-            }
-            // Bas-gauche
-            else if(index==3) {
-                this->downleft->insertBloc(obj);
-                return;
-            }
-            // Bas-droite
-            else if(index==4) {
-                this->downright->insertBloc(obj);
-                return;
-            }   
-        }
-    }
-
-    // S'il n'y a plus de place dans platforms
-    if(this->ind<=4 && this->downleft == nullptr) {
-        this->split();
-    }
-
-    // Et si ça ne loge pas dans le nouveau split Et qu'a plus de place avant ?
-
-    // Sinon ajout
-    else {
-        this->platforms[this->ind]=obj;
-        this->ind++;
-    }
-}
-
-Bloc* Quadtree::collision(Bloc* returnObjets[], Bloc* bloc) {
-    int index = this->searchLeaf(bloc);
-    if(index != -1 && this->downleft != nullptr) {
+    // Si l'objet appartient à un enfant
+    if(!this->isLeaf()) {
         // Haut-gauche
         if(index==1) {
-            this->upleft->collision(returnObjets,bloc);
+            this->upleft->insertBloc(obj);
+            return;
         }
         // Haut-doite
         else if(index==2) {
-            this->upright->collision(returnObjets,bloc);
+            this->upright->insertBloc(obj);
+            return;
         }
         // Bas-gauche
         else if(index==3) {
-            this->downleft->collision(returnObjets,bloc);
+            this->downleft->insertBloc(obj);
+            return;
         }
         // Bas-droite
         else if(index==4) {
-            this->downright->collision(returnObjets,bloc);
+            this->downright->insertBloc(obj);
+            return;
+        }   
+        
+    }
+
+    else {
+        // S'il n'y a plus de place
+        if(this->isFull()) {
+            this->split();
+            for(Bloc bloc : this->blocs) {
+                this->insertBloc(bloc);
+            }
+            blocs.clear();
+            this->insertBloc(obj);
+        }
+        else {
+            this->blocs.push_back(obj);
         }
     }
-
-    for(int i=0; i< this->ind; i++) {
-        returnObjets[i]=this->platforms[i];
-    }
-
-    return returnObjets[0];
 }
+
+/* Recherche blocs dans quadtree la ou se trouve obj */
+std::vector<Bloc> Quadtree::searchBloc(Bloc* obj){
+
+    if(!this->isLeaf()) {
+        // Teste si objet dans cadran du haut
+        if(obj->getPosY() > this->getY()) {
+
+            // A gauche
+            if(obj->getPosX() < this->getX()) {
+                return this->upleft->searchBloc(obj);
+            }
+
+            // A droite
+            else {
+                return this->upright->searchBloc(obj);
+            }
+        }
+        
+        // Test si objet dans cadran du bas
+        else {
+            // A gauche
+            if(obj->getPosX() < this->getX()) {
+                return this->downleft->searchBloc(obj);
+            }
+
+            // A droite
+            else {
+                return this->downright->searchBloc(obj);
+            }
+        }
+    }
+    return this->blocs;
+}
+
+/* Collisions */
